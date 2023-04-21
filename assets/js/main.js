bf75e70b5c917218823bfba04c32682907c69ea0
Felipe Farnetti <Felipefarnetti@gmail.com>
2023-04-21 17:33:13 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded");
+
+  document.querySelector("form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("submit", event);
+
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      subject: document.querySelector("#subject").value,
+      message: document.querySelector("#message").value,
+    };
+
+    console.log(data);
+
+    const response = await axios.post("http://localhost:3000/form", data);
+    console.log(response);
+  });
+});
